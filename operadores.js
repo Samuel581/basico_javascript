8484044c16643ea31dc68d75fbb7a61babf166d1
Samuel581 <00023020@uca.edu.sv>
2023-01-07 21:00:58 -0600

    Operadores

---
+++ operadores.js
@@ -0,0 +1,62 @@
+// Operadores de asignación
+
+let example = 5;
+
+// Operadores de comparación
+// Igualdad
+console.log(example == 5); // true
+
+console.log(example === "5"); // false
+
+// Desigualdad
+console.log(example != 5); // false
+
+// Mayor que
+console.log(example > 5); // false
+
+// Menor que
+console.log(example < 5); // false
+
+// Mayor o igual que
+console.log(example >= 5); // true
+
+// Menor o igual que
+console.log(example <= 5); // true
+
+// Operadores lógicos
+
+// AND
+console.log(example > 5 && example < 10); // false
+
+// OR
+console.log(example > 5 || example < 10); // true
+
+// NOT
+console.log(!(example > 5)); // false
+
+// Operadores de aritmeticos
+// Incremento
+console.log(example++); // 5
+console.log(example); // 6
+
+// Decremento
+console.log(example--); // 6
+console.log(example); // 5
+
+// Suma
+console.log(example + 5); // 10
+
+// Resta
+console.log(example - 5); // 0
+
+// Multiplicación
+console.log(example * 5); // 25
+
+// División
+console.log(example / 5); // 1
+
+// Módulo
+console.log(example % 5); // 0
+
+// Potencia	
+console.log(example ** 5); // 3125
